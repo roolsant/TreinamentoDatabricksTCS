618cf5aebf0c2f172549a84e632eee0908866e19
rodrigosantos2007@gmai.com <rodrigosantos2007@gmai.com>
2025-01-22 15:03:22 +0000

    Inseri Comentatios e Ajustei alguns codigos

--- notebook/raw/1.circuits.py
+++ notebook/raw/1.circuits.py
@@ -1,8 +1,10 @@
 # Databricks notebook source
+### Importando bibliotecas para manipulação de dados
 from pyspark.sql.types import *
 
 # COMMAND ----------
 
+### Definindo o schema para a tabela de dados
 schema = StructType(fields=[StructField("circuitId", IntegerType(), False), 
                      StructField("circuitRef", StringType(), True), 
                      StructField("name", StringType(), True), 
@@ -15,14 +17,33 @@ schema = StructType(fields=[StructField("circuitId", IntegerType(), False),
 
 # COMMAND ----------
 
-df = spark.read.schema(schema).csv("file:/Workspace/Users/user-cqxogaawrjjk@oreilly-cloudlabs.com/TreinamentoDatabricksTCS/landing/circuito.csv", header='true')
+### Lendo os dados da tabela
+
+# Definição do usuário
+User = 'user-rsyscppecwxv@oreilly-cloudlabs.com'
+
+# Construção do caminho do arquivo com interpolação usando f-string
+df = spark.read.schema(schema).csv(
+    f"file:/Workspace/Users/{User}/TreinamentoDatabricksTCS/landing/circuito.csv",
+    header='true'
+)
 
 # COMMAND ----------
 
-df.write.mode("overwrite").format("delta").save("/mnt/treinamento/raw/circuito")
+### Criando um datafrme com os dados
+df.write.mode("overwrite")\
+        .format("delta")\
+        .save("/mnt/treinamento/raw/circuito")
 
 # COMMAND ----------
 
-df_final = spark.read.format("delta").load("/mnt/treinamento/raw/circuito")
+### Leitura do arquivo no formato delta
+df_final = spark.read\
+    .format("delta")\
+    .load("/mnt/treinamento/raw/circuito")
+
+### Exibicao dos dados no dataframe
 df_final.display()
+
+### Imprimindo na tela o schema
 df_final.printSchema()
